ac3f071706569021ef97a56f45bbc15f4d46e63e
Young-ook Kim <youngook.kim@gmail.com>
2022-09-10 09:28:19 +0900

    feat(vpc): upgrade frigga module version

--- labels.tf
+++ labels.tf
@@ -1,7 +1,7 @@
 ### frigga name
 module "frigga" {
   source  = "Young-ook/spinnaker/aws//modules/frigga"
-  version = "2.3.3"
+  version = "2.3.5"
   name    = var.name == null || var.name == "" ? "vpc" : var.name
   petname = true
 }
