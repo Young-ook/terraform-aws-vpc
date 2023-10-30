03e5f1bffcbe416d0168b0b34b4cfcf8b1578714
Young-ook Kim <youngook.kim@gmail.com>
2023-10-30 16:37:45 +0900

    fix(vpc): revert vpc eip type change and update the version constraints for eip type

---
+++ versions.tf
@@ -0,0 +1,15 @@
+## requirements
+
+terraform {
+  # Terraform test is an experimental features introduced in Terraform CLI v0.15.0.
+  # So, you'll need to upgrade to v0.15.0 or later to use terraform test.
+  required_version = ">= 0.15"
+
+  # The 'domain' parameter was migrated from a 'Computed' attribute to an input attribute in the aws_eip resource in version 5.0.0
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
